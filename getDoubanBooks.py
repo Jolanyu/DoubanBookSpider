aab9bafbdf46fa44afe888004738258354f789e8
Jolan yu <1258032547@qq.com>
2021-12-23 09:32:22 +0800

    12/23 update

--- getDoubanBooks.py
+++ getDoubanBooks.py
@@ -111,9 +111,9 @@ def getBookUrl(html):
 
 # https://book.douban.com/tag/%E5%84%BF%E7%AB%A5%E6%96%87%E5%AD%A6?start=0&type=S
 if __name__ == "__main__":
-    startPage = 16
+    startPage = 32
     endPage = 49
-    bookClass = "政治"
+    bookClass = "美食"
     bookClassCode = urllib.parse.quote(bookClass)
     for i in range(startPage, endPage+1):
         url = "https://book.douban.com/tag/"+bookClassCode+"?start=" + \
@@ -122,6 +122,7 @@ if __name__ == "__main__":
         html = getHtml(url)
         urlList = getBookUrl(html)
         print(urlList)
+        time.sleep(5)
         for url in urlList:
             time.sleep(5)
             html = getHtml(url)
